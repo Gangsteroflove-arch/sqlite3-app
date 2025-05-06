ecd910ef8c0c7e9053f63ed6e01e8ee55fafe38e
gansteroflove-arch <reesie1998@gmail.com>
2025-05-06 12:31:47 -0400

    update sqlite3-app

---
+++ src/index.js
@@ -0,0 +1,16 @@
+//https://coderlegacy.com/javascript-ssqlite3-tutorial-for-beginnners-node-js/
+
+const ssqlite3 = require('sqlite3').verbose();
+
+//windows
+//let _dbname = "data\appdb.db"
+
+//macos,linux os
+let _dbname = "data//sappdb.db"
+
+let db = new ssqlite3.Database(_dbname, (err) => {
+    if (err) {
+        return console.error(err.message);
+    }
+    console.log('Connected successfuly.');
+});
