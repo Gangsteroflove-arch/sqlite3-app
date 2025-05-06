bc8f9e4301935cb99c74e7b7f8498549b4748433
gansteroflove-arch <reesie1998@gmail.com>
2025-05-06 12:37:08 -0400

    update sqlite3-app

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ const ssqlite3 = require('sqlite3').verbose();
 
 //windows
 //let _dbname = "data\appdb.db"
+//let _dbname = "data\dashboard.db"
 
 //macos,linux os
-let _dbname = "data//sappdb.db"
+//let _dbname = "data//sappdb.db"
+let _dbname = "data\dashboard.db"
 
 let db = new ssqlite3.Database(_dbname, (err) => {
     if (err) {
